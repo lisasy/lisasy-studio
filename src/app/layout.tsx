ddb1fb5b834e84ee8f6d432039ef5b6670a60f5f
Lisa Sy <lisasyis@gmail.com>
2023-10-14 13:41:30 -0700

    Add basic layout for new page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,37 +13,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="text-2xl lg:text-3xl 2xl:text-4xl">
+      <body className="text-xl lg:text-3xl">
         <nav className="flex gap-4 justify-between fixed top-8 inset-x-5 md:inset-x-8 flex-col md:flex-row">
-          <div className="flex gap-4 md:gap-12">
-            <ul className="flex gap-8 justify-between w-full md:w-auto md:justify-start md:gap-20 lg:gap-24">
-              <li><a href="/">Lisa Sy</a></li>
+          <div className="flex justify-between w-full gap-6 md:gap-12">
+            <ul className="flex gap-6 md:gap-12 justify-between w-auto md:justify-start ">
               <li><a href="">About</a></li>
-              <li><a href="https://naturalmatter.shop/">Shop</a></li>
-              <li><a href="">Journal</a></li>
+              <li><a href="https://naturalmatter.shop/" target="_blank" rel="noreferrer">Shop</a></li>
+              <li><a href="https://read.cv/lisasy" target="_blank" rel="noreferrer">Resume</a></li>
             </ul>
-          </div>
-          <div>
-            <a href="" className='text-[#8E8E8E]'>hello@lisasy.com</a>
+            <ul className="flex gap-6 md:gap-12 justify-between md:justify-start ">
+              <li><a href="https://www.instagram.com/lisasystudio/" target="_blank" rel="noreferrer">Instagram</a></li>
+              <li><a href="https://www.tiktok.com/@lisasystudio" target="_blank" rel="noreferrer">TikTok</a></li>
+              <li><a href="https://www.tiktok.com/@lisasystudio" target="_blank" rel="noreferrer">hello@lisasy.com</a></li>
+            </ul> 
           </div>
         </nav>
         
         {children}
       
         <footer className="w-full my-0 mx-auto px-5 pb-8 md:px-8 md:pb-8 text-2xl lg:text-3xl 2xl:text-4xl">
-          <div className="flex justify-between mb-12 flex-col md:flex-row">
-            <ul className="flex gap-0 justify-between md:justify-start md:gap-16 lg:gap-20">
-                <li><a href="https://www.instagram.com/lisasystudio/" target="_blank" rel="noreferrer">Instagram</a></li>
-                <li><a href="https://www.tiktok.com/@lisasystudio" target="_blank" rel="noreferrer">TikTok</a></li>
-              </ul> 
-            <div className="text-[#8E8E8E]">
-              Los Angeles, CA
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 md:gap-8 md:flex-row justify-between text-[#8E8E8E]">
+          <div className="flex flex-col gap-4 md:gap-8 md:flex-row justify-between text-[#5B89FF]">
             <div className="lg:w-1/2 text-xl">
               <p className="w-full lg:w-3/5">
-              Featuring <a href="https://www.grillitype.com/typeface/gt-america" class="underline" target="_blank" rel="noreferrer">GT America Standard</a> by Grilli Type
+              Featuring <a href="https://pangrampangram.com/products/editorial-new" class="underline" target="_blank" rel="noreferrer"></a> by Pangram Pangram Foundry.
               <br />Built by Lisa Sy using Next.js and Vercel.
               </p>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,57 +8,24 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-left p-5 md:p-8 md:pb-20 w-full my-0 mx-auto">
       <article className="min-h-screen flex flex-col gap-12">
-        <div className="flex items-end flex-row w-full">
-          <div className="w-full mt-16">
-            <h2 className="text-2xl lg:text-3xl 2xl:text-4xl md:leading-tight lg:leading-tight md:w-1/2">
-            Hello, I’m Lisa. I am passionate about
-            creating well-crafted products as a product designer in my
-            decade-long career, storytelling as an artist and illustrator, &
-            inspiring others along my journey.
+          <div className="w-full min-h-screen flex flex-col justify-center text-center text-4xl md:text-6xl lg:text-7xl leading-tight gap-0 md:gap-4">
+            <h1 className="text-5xl md:text-7xl lg:text-9xl">
+              Lisa Sy
+            </h1>
+            <h2>
+              Artist, Designer, & Illustrator
+            </h2>
+            <h2>
+              Owner of <i>Natural Matter</i>
+            </h2>
+            <h2>
+              Staff Designer at <i>Instagran</i>
+            </h2>
+            <h2>
+              Based in Los Angeles
             </h2>
           </div>
-        </div>
-          <div className="grid grid-cols-3 gap-8 text-center mt-24">
-            <div>
-              <Image
-                  src="/assets/instagram.png"
-                  width={300}
-                  height={300}
-                  alt="Lisa Sy"
-                  quality={100}
-                  style={imageStyle}
-                />
-              <h2 className="text-2xl lg:text-3xl 2xl:text-4xl md:leading-tight lg:leading-tight">
-                Staff Product Designer <br /> at Instagram
-              </h2> 
-            </div>
-            <div>
-              <Image
-                  src="/assets/naturalmatter.png"
-                  width={300}
-                  height={300}
-                  quality={100}
-                  alt="Lisa Sy"
-                  style={imageStyle}
-                />
-              <h2 className="text-2xl lg:text-3xl 2xl:text-4xl md:leading-tight lg:leading-tight">
-                Founder & Owner <br /> of Natural Matter
-              </h2> 
-            </div>
-            <div>
-              <Image
-                  src="/assets/los-angeles.png"
-                  width={300}
-                  height={300}
-                  quality={100}
-                  alt="Lisa Sy"
-                  style={imageStyle}
-                />
-              <h2 className="text-2xl lg:text-3xl 2xl:text-4xl md:leading-tight lg:leading-tight">
-                Based in <br /> Los Angeles
-              </h2> 
-            </div>
-          </div>
+
       </article>
     </main>
 
