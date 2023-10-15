644dac4616c86324edc16d1488bbe6ae4e036ac2
Lisa Sy <lisasyis@gmail.com>
2023-10-15 16:22:06 -0700

    Make update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-export const metadata = {
-  title: 'Lisa Sy · Design & Art',
-  description: 'Lisa Sy is an artist & product designer based in Los Angeles.',
-}
 
 export default function RootLayout({
   children,
@@ -14,28 +8,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="text-xl lg:text-3xl">
-        <nav className="flex gap-4 justify-between fixed top-8 inset-x-5 md:inset-x-8 flex-col md:flex-row">
-          <div className="flex justify-between w-full gap-6 md:gap-12">
-            <ul className="flex gap-6 md:gap-12 justify-between w-auto md:justify-start ">
-              <li><a href="">About</a></li>
-              <li><a href="https://naturalmatter.shop/" target="_blank" rel="noreferrer">Shop</a></li>
-              <li><a href="https://read.cv/lisasy" target="_blank" rel="noreferrer">Resume</a></li>
-            </ul>
-            <ul className="flex gap-6 md:gap-12 justify-between md:justify-start ">
-              <li><a href="https://www.instagram.com/lisasystudio/" target="_blank" rel="noreferrer">Instagram</a></li>
-              <li><a href="https://www.tiktok.com/@lisasystudio" target="_blank" rel="noreferrer">TikTok</a></li>
-              <li><a href="https://www.tiktok.com/@lisasystudio" target="_blank" rel="noreferrer">Email</a></li>
-            </ul> 
-          </div>
-        </nav>
+       
         
         {children}
       
         <footer className="w-full my-0 mx-auto px-5 pb-8 md:px-8 md:pb-8 text-2xl lg:text-3xl 2xl:text-4xl">
           <div className="flex flex-col gap-4 md:gap-8 md:flex-row justify-between text-[#5B89FF]">
             <div className="lg:w-1/2 text-xl">
-              <p className="w-full lg:w-3/5">
-              Featuring <a href="https://pangrampangram.com/products/editorial-new" class="underline" target="_blank" rel="noreferrer"></a> by Pangram Pangram Foundry.
+              <p className="w-full">
+              Featuring <a href="https://pangrampangram.com/products/editorial-new" className="underline" target="_blank" rel="noreferrer">Editorial</a> by Pangram Pangram Foundry.
               <br />Built by Lisa Sy using Next.js and Vercel.
               </p>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import Navigation from '/components/Navigation.js';
+
 
 const imageStyle = {
   margin: "0 auto",
@@ -6,9 +8,10 @@ const imageStyle = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-left p-5 md:p-8 md:pb-20 w-full my-0 mx-auto">
-      <article className="min-h-screen flex flex-col gap-12">
-          <div className="w-full min-h-screen flex flex-col justify-center text-center text-4xl md:text-6xl lg:text-7xl leading-tight gap-0 md:gap-4">
+    <main className="flex min-h-[50vh] md:min-h-screen flex-col items-left p-5 md:p-8 md:pb-20 w-full my-0 mx-auto">
+       <Navigation />
+      <article className="min-h-[50vh] md:min-h-screen flex flex-col gap-12">
+          <div className="w-full min-h-[50vh] md:min-h-screen flex flex-col justify-center text-center text-4xl md:text-6xl lg:text-7xl leading-tight gap-0 md:gap-4">
             <h1 className="text-5xl md:text-7xl lg:text-9xl">
               Lisa Sy
             </h1>
@@ -19,10 +22,8 @@ export default function Home() {
               Owner of <i>Natural Matter</i>
             </h2>
             <h2>
-              Staff Designer at
-              <i>
-                Instagram
-                <div className="absolute w-[200px] rotate-[12deg] top-[300px] -right-[10px] hover:rotate-0 hover:cursor-pointer hover:scale-125 transition transition-timing-function: linear;">
+              Staff Designer at <i className="ig-label">Instagram
+                <div className="absolute w-[140px] md:w-[200px] rotate-[12deg] top-[480px] -right-[30px] md:top-[300px] md:-right-[10px] hover:rotate-0 hover:cursor-pointer hover:scale-125 transition transition-timing-function: linear;">
                   <Image
                     src={'/assets/ig.gif'}
                     layout={'responsive'}
@@ -36,7 +37,7 @@ export default function Home() {
             </h2>
             <h2>
               Based in Los Angeles
-              <div className="absolute w-[200px] -rotate-[12deg] top-[120px] -left-[10px] hover:rotate-0 hover:cursor-pointer hover:scale-125 transition transition-timing-function: linear;">
+              <div className="absolute w-[140px] md:w-[200px] -rotate-[12deg] top-[80px] md:top-[120px] -left-[10px] hover:rotate-0 hover:cursor-pointer hover:scale-125 transition transition-timing-function: linear;">
                 <Image
                   src={'/assets/la.gif'}
                   layout={'responsive'}
