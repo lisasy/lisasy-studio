d28a65edff948b0addf44d9de266b84b719cfbe7
Lisa Sy <lisasyis@gmail.com>
2025-03-09 22:47:33 -0700

    initial march 2025 update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,22 +7,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="text-xl lg:text-2xl">
+      <body className="flex flex-col min-h-screen justify-between">
        
         
         {children}
       
-        <footer className="w-full my-0 mx-auto px-5 pb-8 md:px-8 md:pb-8 text-xl lg:text-2xl 2xl:text-3xl text-[#51D696]">
+        <footer className="w-full my-0 mx-auto px-5 pb-8 md:px-8 md:pb-8 text-sm md:text-md lg:text-lg">
           <div className="flex flex-col gap-4 md:gap-8 md:flex-row justify-between">
-            <div className="lg:w-1/2 text-xl">
+            <div className="lg:w-1/2">
               <p className="w-full">
               Featuring <a href="https://pangrampangram.com/products/editorial-new" className="underline" target="_blank" rel="noreferrer">GT America</a> by Pangram Pangram Foundry.
               <br />Built by Lisa Sy using Next.js and Vercel.
               </p>
             </div>
-            <div className="text-xl lg:w-1/2 md:text-right">
+            <div className="lg:w-1/2 md:text-right">
               <p>Remember to be a kind person.</p>
-              <p>Copyright 2023</p>
+              <p>Copyright 2025</p>
             </div>
           </div>
         </footer>
--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,22 @@
-import Image from 'next/image'
-import Navigation from '/components/Navigation.js';
-
-
-const imageStyle = {
-  margin: "0 auto",
-}
-
 export default function Home() {
   return (
-    <main className="flex min-h-[50vh] md:min-h-screen flex-col items-left p-5 md:p-8 md:pb-20 w-full my-0 mx-auto">
-       <Navigation />
-      <article className="min-h-[50vh] md:min-h-screen flex flex-col gap-12">
-          <div className="w-full min-h-[50vh] md:min-h-screen flex flex-col justify-center text-left text-2xl md:text-3xl lg:text-4xl leading-tight gap-0 md:gap-4">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl">
-              Lisa Sy
+    <main className="flex flex-col items-left p-5 md:p-8 md:pb-20 w-full my-0 mx-auto">
+      <article className="flex flex-col gap-12">
+          <div className="w-full sm:w-3/5 flex flex-col justify-center text-left text-lg md:text-xl lg:text-2xl leading-[1.33] gap-4 md:gap-7">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl">
+              Hi, I&apos;m Lisa.
             </h1>
-            <h2>
-              Artist, Designer, & Illustrator in Los Angeles
-            </h2>
-            <h2>
-              Founder and Owner of <a href="https://www.naturalmatter.shop/" class="underline">Natural Matter</a>
-            </h2>
-            <h2>
-              Staff Product Designer at <a href="https://www.instagram.com" class="underline">Instagram</a>
-            </h2>
-              <div className="absolute w-[140px] md:w-[200px] -rotate-[12deg] top-[300px] md:top-[360px] right-[200px] hover:rotate-0 hover:cursor-pointer hover:scale-125 transition transition-timing-function: linear;">
-                <Image
-                  src={'/assets/la.gif'}
-                  layout={'responsive'}
-                  height={175}
-                  width={175}
-                  alt={`los angeles`}
-                  unoptimized={true}
-                />
-            </div>
+            <p className="leading-[1.33]">
+              I&apos;m an experienced product designer passionate about partnering with collaborators to bring ideas to life. <span className="text-bold text-[#9F9915]">Currently, I&apos;m available for new projects and opportunities starting in April 2025.</span>
+            </p>
+            <p className="leading-[1.33]">
+              Most recently, I was a Staff Product Designer at Meta & Instagram. As a generalist designer, I spent the past 13 years working across multiple aspects of design (including research, product management, and front-end development) in a wide breadth of environments and domains.
+            </p>
+            <p className="leading-[1.33]">
+              I&apos;m reachable via <a href="mailto:hello@lisasy.com" className="underline">email</a>. My resume is available <a href="https://read.cv/lisasy" className="underline" target="_blank" rel="noreferrer">here</a>.
+            </p>
           </div>
-
       </article>
     </main>
-
-
   )
 }
