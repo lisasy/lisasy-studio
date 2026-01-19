96571844d461a5bb7db1c90bab88f2276c660ea6
Lisa Sy <lisasyis@gmail.com>
2026-01-19 15:29:47 -0800

    Update artist statement

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,13 +7,12 @@ export default function About() {
       <div className=" text-left text-lg md:text-xl lg:text-2xl leading-[1.33] gap-4 md:gap-7">
         <h3 className="mb-5">About</h3>
         <p>
-          Lisa Sy is an artist and illustrator fascinated by the natural world and our attention to it. Based in Southern
-          California, she is attuned to capturing moments in space and time,
+        My name is Lisa Sy and I'm an artist, painter, illustrator, and writer based in Los Angeles, CA. I'm fascinated by the natural world and our attention to it. I'm attuned to capturing moments in space and time,
           portrayed through plein air painting, naturalist documentation, and quiet reflection.
         </p>
         <TwoColumn title="Artist Statement">
           <p className="smaller">
-            My work is inspired by the natural world - its moments, its subtleties, and its lessons. Through my art, I strive to capture and amplify the beauty of these often overlooked and under-appreciated moments by pointing attention to them. I hope my art invites you to pause, reflect, and find wonder in the seemingly ordinary.
+           My work is inspired by the natural world and our relationship to it. Working in plein air, I often paint and draw natural and built urban landscapes, ranging from desert and mountainscapes, to bustling city plazas replete with people. By intentionally inserting myself into a place as an observer and painter, I slow down to notice what's easily missed—the quality of light at a particular hour, the way a place composes itself , the life of a seemingly static scene. Through my art, I strive to capture and amplify the beauty of these often overlooked and under-appreciated moments by pointing attention to them. I hope my art invites you to pause, reflect, and find wonder in the seemingly ordinary. 
           </p>
         </TwoColumn>
         <TwoColumn title="Education">
